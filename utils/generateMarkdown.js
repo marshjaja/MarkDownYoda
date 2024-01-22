f3fdc4ce4dff54752d63243107e4e71f260d74e5
marshjaja <114920895+marshjaja@users.noreply.github.com>
2024-01-22 14:37:48 +0000

    updated files

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+	return `
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+  
 
 `;
 }
